5ec7691d8689e7f177140ea6755cbce2006e1d6e
Stefan Ilievski <stefan_ilievski994@hotmail.com>
2023-07-25 12:29:46 +0200

    Animating with requestAnimationFrame

--- script.js
+++ script.js
@@ -91,9 +91,6 @@ function createCanvas() {
   renderCanvas();
 }
 
-// Remove this
-createCanvas();
-
 // Reset Ball to Center
 function ballReset() {
   ballX = width / 2;
@@ -206,6 +203,7 @@ function animate() {
   ballMove();
   ballBoundaries();
   computerAI();
+  window.requestAnimationFrame(animate);
 }
 
 // Start Game, Reset Everything
@@ -221,7 +219,6 @@ function startGame() {
   createCanvas();
   animate();
   canvas.addEventListener("mousemove", (e) => {
-    console.log(e.clientX);
     playerMoved = true;
     // Compensate for canvas being centered
     paddleBottomX = e.clientX - canvasPosition - paddleDiff;
@@ -237,4 +234,4 @@ function startGame() {
 }
 
 // On Load
-// startGame();
+startGame();
